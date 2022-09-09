1a6962e3db4c96ec75c98ada488574d131284791
Ganning Xu <ganningxu@gmail.com>
2022-09-08 21:19:44 -0400

    Deploy gh-pages

---
+++ out/_next/static/N0dQWvQ18FsytPZknyC84/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/545f34e4-b36b7439aab72b08.js","static/chunks/351-83727b3bba59d072.js","static/chunks/pages/index-5351e7e6d82fa63b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
