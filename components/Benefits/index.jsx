55add1c26127844c6b8bd0bffdd19e2c7aadf11c
Ganning Xu <ganningxu@gmail.com>
2022-09-08 21:20:12 -0400

    added 2023 update with activities and meeting time

--- components/Benefits/index.jsx
+++ components/Benefits/index.jsx
@@ -1,22 +1,126 @@
-import { Box, Heading, Container, SimpleGrid, Link, Text } from '@chakra-ui/react'
-import { Card } from './Card'
+import {
+  Box,
+  Heading,
+  Container,
+  SimpleGrid,
+  Link,
+  Text,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/react";
+import { Card } from "./Card";
 
 export function Benefits() {
-    return (
-        <Box bg='gray.100' p={4} color='gray.800' pt="6">
-            <Container maxW="container.xl">
-                <Heading as='h1' fontSize={{ base: "2xl", md: "3xl", lg: "5xl" }} color="blue.700" fontWeight="bolder" textAlign="center">Our Activities</Heading>
-                <Box maxW="700px" mx="auto" p={4} boxShadow="lg" bg="white" rounded="lg" mb="8">
-                    <Text>CyberUnicorns is a student-led club at NCSSM dedicated to cybersecurity and it&apos;s opportunities. CyberUnicorns strives to engage the NCSSM community with cybersecurity through education, capture the flag challenges, possible scholarships. By teaching others about cybersecurity, we hope to inform them about the various tactics of hackers and how they can keep their own websites and even themselves safe. Scroll down to join us!</Text>
-                </Box>
-                <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing="30px" mt="4">
-                    <Card title="Learn Cybersecurity" logo="https://www.pngall.com/wp-content/uploads/4/Cyber-Security-PNG-Clipart.png">Cybersecurity is one of the fastest growing career paths in the world. With techniques through programming, web exploitation, linux tools, and cryptography, there&apos;s so much to learn about this fascinating topic!</Card>
-                    <Card title="CyberStart America" logo="https://d33wubrfki0l68.cloudfront.net/2354660573da025dc539425c1a7ce325b437524e/fc06b/images/csa-web-product-02.png">A capture the flag compeition that students from across the United States compete in. CyberStart America is the most enjoyable way to discover your talent, advance your skills and win scholarships in cybersecurity! Click <Link href="https://www.cyberstartamerica.org/" color="blue.400" isExternal>here</Link> to learn more.</Card>
-                    <Card title="picoCTF" logo="/pico.png">Developed by Carnegie Mellon, picoCTF is a capture the flag cybersecurity game for high school students. Join CyberUnicorns as we tackle various problems from this competition!</Card>
-                    <Card title="Scholarships" logo="https://d33wubrfki0l68.cloudfront.net/2a411dd613ce138af90cb6ccacaa2a34a1b8df59/3f54b/images/csa-web-badge-ncsf.png">With CyberUnicorns, you can win scholarships from The National Cyber Scholarship Foundation. With award levels for Semifinalist, Finalist, National Cyber Scholar, and National Cyber Scholar with honors, it&apos;s not too difficult to boost your college resume! </Card>
-                </SimpleGrid>
+  return (
+    <Box bg="gray.100" p={4} color="gray.800" pt="6">
+      <Container maxW="container.xl">
+        <Heading
+          as="h1"
+          fontSize={{ base: "2xl", md: "3xl", lg: "5xl" }}
+          color="blue.700"
+          fontWeight="bolder"
+          textAlign="center"
+        >
+          Our Activities
+        </Heading>
+        <Box
+          maxW="700px"
+          mx="auto"
+          p={4}
+          boxShadow="lg"
+          bg="white"
+          rounded="lg"
+          mb="8"
+        >
+          <Text>
+            CyberUnicorns is a student-led club at NCSSM dedicated to
+            cybersecurity and it&apos;s opportunities. CyberUnicorns strives to
+            engage the NCSSM community with cybersecurity through education,
+            capture the flag challenges, possible scholarships. By teaching
+            others about cybersecurity, we hope to inform them about the various
+            tactics of hackers and how they can keep their own websites and even
+            themselves safe. Scroll down to join us!
+          </Text>
 
-            </Container>
-        </Box >
-    )
+          <Text fontWeight="bold" mt={4}>
+            Here are some of our recent activities!
+          </Text>
+
+          <UnorderedList>
+            <ListItem>
+              <Link
+                href="https://github.com/CyberUnicorns/Intro-CTF-Challenges-2023"
+                color="blue.300"
+                isExternal
+              >
+                Intro to CTF challenges 2023 (8 challenges)
+              </Link>
+            </ListItem>
+
+            <ListItem>
+              <Link
+                href="https://github.com/CyberUnicorns/Intro-CTF-Challenges"
+                color="blue.300"
+                isExternal
+              >
+                Intro to CTF challenges 2022 (5 challenges)
+              </Link>
+            </ListItem>
+
+            <ListItem>
+              <Link
+                href="https://github.com/CyberUnicorns/Curriculum"
+                color="blue.300"
+                isExternal
+              >
+                CyberUnicorns Curriculum!
+              </Link>
+            </ListItem>
+          </UnorderedList>
+        </Box>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing="30px" mt="4">
+          <Card
+            title="Learn Cybersecurity"
+            logo="https://www.pngall.com/wp-content/uploads/4/Cyber-Security-PNG-Clipart.png"
+          >
+            Cybersecurity is one of the fastest growing career paths in the
+            world. With techniques through programming, web exploitation, linux
+            tools, and cryptography, there&apos;s so much to learn about this
+            fascinating topic!
+          </Card>
+          <Card
+            title="CyberStart America"
+            logo="https://d33wubrfki0l68.cloudfront.net/2354660573da025dc539425c1a7ce325b437524e/fc06b/images/csa-web-product-02.png"
+          >
+            A capture the flag compeition that students from across the United
+            States compete in. CyberStart America is the most enjoyable way to
+            discover your talent, advance your skills and win scholarships in
+            cybersecurity! Click{" "}
+            <Link
+              href="https://www.cyberstartamerica.org/"
+              color="blue.400"
+              isExternal
+            >
+              here
+            </Link>{" "}
+            to learn more.
+          </Card>
+          <Card title="picoCTF" logo="/pico.png">
+            Developed by Carnegie Mellon, picoCTF is a capture the flag
+            cybersecurity game for high school students. Join CyberUnicorns as
+            we tackle various problems from this competition!
+          </Card>
+          <Card
+            title="Scholarships"
+            logo="https://d33wubrfki0l68.cloudfront.net/2a411dd613ce138af90cb6ccacaa2a34a1b8df59/3f54b/images/csa-web-badge-ncsf.png"
+          >
+            With CyberUnicorns, you can win scholarships from The National Cyber
+            Scholarship Foundation. With award levels for Semifinalist,
+            Finalist, National Cyber Scholar, and National Cyber Scholar with
+            honors, it&apos;s not too difficult to boost your college resume!{" "}
+          </Card>
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
 }
--- components/Join/index.jsx
+++ components/Join/index.jsx
@@ -1,33 +1,87 @@
-import { Box, Heading, Container, SimpleGrid, Text, Link } from '@chakra-ui/react'
-import { Numbered } from './Numbered'
-import { UnicornBullet } from './UnicornBullet'
-
+import {
+  Box,
+  Heading,
+  Container,
+  SimpleGrid,
+  Text,
+  Link,
+} from "@chakra-ui/react";
+import { Numbered } from "./Numbered";
+import { UnicornBullet } from "./UnicornBullet";
 
 export function Join() {
-    return (
-        <Box bg="gray.100">
-            <Container maxW="container.xl" pt="4">
-                <Heading as='h1' fontSize={{ base: "2xl", md: "3xl", lg: "5xl" }} color="blue.700" fontWeight="bolder" textAlign="center">Join Us</Heading>
-                <SimpleGrid columns={{ base: 1, lg: 2 }}>
-                    <Box bg="white" px="3" boxShadow="xl" rounded="md" maxW="700px" mx="auto" mt="4" d="flex" alignItems="center">
-                        <Box maxW="420px">
-                            <UnicornBullet text="All experience levels welcome" />
-                            <UnicornBullet text="Gain leadership positions" />
-                            <UnicornBullet text="Win Scholarships" />
-                            <UnicornBullet text="Learn about programming, hacking, and cryptography" />
-                            <UnicornBullet text="Solve capture-the-flag challenges" />
-                        </Box>
-                    </Box>
-                    <Box align="center" bg="white" p={8} boxShadow="xl" rounded="md" maxW="500px" mx="auto" mt="4">
-                        <Numbered number="1">Fill out the <Link href="https://docs.google.com/forms/d/e/1FAIpQLSdbwMFp5wgEvTBW75CwoPy9aCblNMQQsjjVm6yzGxV9WfXbUg/viewform?usp=sf_link" color="blue.300" isExternal>member information form</Link></Numbered>
-                        <Numbered number="2">Come to one of our meetings!</Numbered>
-                        <Numbered number="3">Join our <Link href="https://www.facebook.com/groups/1331698317252357/" color="blue.300" isExternal>Facebook Group</Link></Numbered>
-                        <Text fontWeight="bold" fontSize="3xl" color="orange.400">Mondays @ 5-6 PM</Text>
-                        <Text fontWeight="bold" fontSize="3xl" color="blue.700">Room: Beall 7</Text>
-
-                    </Box>
-                </SimpleGrid>
-            </Container >
-        </Box >
-    )
+  return (
+    <Box bg="gray.100">
+      <Container maxW="container.xl" pt="4">
+        <Heading
+          as="h1"
+          fontSize={{ base: "2xl", md: "3xl", lg: "5xl" }}
+          color="blue.700"
+          fontWeight="bolder"
+          textAlign="center"
+        >
+          Join Us
+        </Heading>
+        <SimpleGrid columns={{ base: 1, lg: 2 }}>
+          <Box
+            bg="white"
+            px="3"
+            boxShadow="xl"
+            rounded="md"
+            maxW="700px"
+            mx="auto"
+            mt="4"
+            d="flex"
+            alignItems="center"
+          >
+            <Box maxW="420px">
+              <UnicornBullet text="All experience levels welcome" />
+              <UnicornBullet text="Gain leadership positions" />
+              <UnicornBullet text="Win Scholarships" />
+              <UnicornBullet text="Learn about programming, hacking, and cryptography" />
+              <UnicornBullet text="Solve capture-the-flag challenges" />
+            </Box>
+          </Box>
+          <Box
+            align="center"
+            bg="white"
+            p={8}
+            boxShadow="xl"
+            rounded="md"
+            maxW="500px"
+            mx="auto"
+            mt="4"
+          >
+            <Numbered number="1">
+              Fill out the{" "}
+              <Link
+                href="https://docs.google.com/forms/d/e/1FAIpQLSdbwMFp5wgEvTBW75CwoPy9aCblNMQQsjjVm6yzGxV9WfXbUg/viewform?usp=sf_link"
+                color="blue.300"
+                isExternal
+              >
+                member information form
+              </Link>
+            </Numbered>
+            <Numbered number="2">Come to one of our meetings!</Numbered>
+            <Numbered number="3">
+              Join our{" "}
+              <Link
+                href="https://www.facebook.com/groups/1331698317252357/"
+                color="blue.300"
+                isExternal
+              >
+                Facebook Group
+              </Link>
+            </Numbered>
+            <Text fontWeight="bold" fontSize="3xl" color="orange.400">
+              Thursdays @ 9-9:40 PM
+            </Text>
+            <Text fontWeight="bold" fontSize="3xl" color="blue.700">
+              Room: Hill 10
+            </Text>
+          </Box>
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
 }
